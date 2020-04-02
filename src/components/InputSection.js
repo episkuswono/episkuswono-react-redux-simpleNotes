a773bf29aab2703e5d7103ff87ded3e73efb5328
episkuswono <episkuswono@gmail.com>
2020-04-02 14:30:52 +0700

    value onchange and click button, for button and text, textarea

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import noteActions from '../redux/actions/noteAction';
+import inputActions from '../redux/actions/inputActions';
 import './InputSection.style.scss';
 
 const InputSection = () => {
@@ -10,10 +11,29 @@ const dispatch = useDispatch();
 
     return(
         <div className="InputSection__container">
-            <input type="text" placeholder="Note title"/>
-            <textarea placeholder="Note content">
+            <input type="text" 
+            placeholder="Note title"
+            value={title}
+            onChange={e => 
+                dispatch(inputActions.setInputTitle(e.target.value))
+            }
+            />
+            <textarea 
+            placeholder="Note content"
+            value={content}
+            onChange={e => 
+                dispatch(inputActions.setInputContent(e.target.value))
+            }
+            >    
             </textarea>
-            <button>
+            <button
+                onClick={() => {
+                    dispatch(noteActions.addNote({
+                        title,
+                        content
+                    }))
+                }}
+            >
                 ADD NOTE
             </button>
         </div>
--- src/redux/actions/noteAction.js
+++ src/redux/actions/noteAction.js
@@ -2,16 +2,16 @@ import actionTypes from '../actionTypes';
 
 export default{
     addNote: (note) => ({
-        type: actionTypes.ADD_TODO,
+        type: actionTypes.ADD_NOTE,
         note
     }),
     updateNote: (index, note) => ({
-        type: actionTypes.UPDATE_TODO,
+        type: actionTypes.UPDATE_NOTE,
         index,
         note,
     }),
     deleteNote: (index) =>({
-        type: actionTypes.REMOVE_TODO,
+        type: actionTypes.REMOVE_NOTE,
         index
     })
 }
